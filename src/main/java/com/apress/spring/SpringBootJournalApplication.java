01ba32eeb01c1bc1cc16e74adb661d28c918b67d
pianorange123 <pianorange123@gmail.com>
2018-03-10 17:28:06 +0900

    spring boot

--- src/main/java/com/apress/spring/SpringBootJournalApplication.java
+++ src/main/java/com/apress/spring/SpringBootJournalApplication.java
@@ -1,10 +1,23 @@
 package com.apress.spring;
 
+import com.apress.spring.domain.Journal;
+import com.apress.spring.repository.JournalRepository;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringBootJournalApplication {
+
+	@Bean
+	InitializingBean saveData(JournalRepository repo){
+		return () -> {
+			repo.save(new Journal("스프링부트 입문","오늘부터 스프링 공부를 시작했다","02/20/2017"));
+			repo.save(new Journal("할일이 아주많다","많이 졸리다","02/20/2017"));
+			repo.save(new Journal("빨리끝내라","언제끝나냐","02/20/2017"));
+		};
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootJournalApplication.class, args);
--- src/main/java/com/apress/spring/web/JournalController.java
+++ src/main/java/com/apress/spring/web/JournalController.java
@@ -1,10 +1,19 @@
 package com.apress.spring.web;
 
+import com.apress.spring.domain.Journal;
 import com.apress.spring.repository.JournalRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class JournalController {
@@ -14,12 +23,42 @@ public class JournalController {
 
     @RequestMapping("/")
     public String index(Model model){
+        //testbranch pull request test
+        //testbranch pull request test
+        //testbranch pull request test
+        //testbranch pull request test
+        //testbranch pull request test
+        //testbranch pull request test
+        //testbranch pull request test
+        //testbranch pull request test
+        //testbranch pull request test
+        //testbranch pull request test
+        //testbranch pull request test
+        //testbranch pull request test
+        //testbranch pull request test
+        //testbranch pull request test
+
+
         //모델 객체 생성 "journal"이라는 이름으로 repo.findAll() 속성추가
-        model.addAttribute("journal",repo.findAll());
         //repo.findAll은 JpaRepository 기본 메서드.
         //JpaRepository에는 findAll, save, flush,deleteVatch같은 상용메서드 10개정도 선언되있다.
+        model.addAttribute("journal",repo.findAll());
 
         //마지막으로 index 추가해주면 스프링 MVC엔진은 템플릿 폴더에서 index.html 파일을 찾는다.
         return "index";
+    }
+
+    @GetMapping("/hello")
+    @ResponseBody
+    public String hello(@RequestParam Optional<String> name) {
+        //@RequestParam (required = false ) 반드시 들어와야하는지
+        //@Responsebody , Controller 합친것 문자열 제이슨 리턴
+
+        return name.orElse("guest ") + " hello!";
+    }
+
+    @RequestMapping(value="/journal", produces = { MediaType.APPLICATION_JSON_VALUE } )
+    public @ResponseBody List<Journal> getJournal() {
+        return repo.findAll();
     }
 }
